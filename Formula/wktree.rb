4179646c70edcd3f343a245923446d6b151ef7cd
codethread <adamhalldesigns@gmail.com>
2026-08-03 06:48:39 +0100

    chore(homebrew): update formula to v0.2.1

--- Formula/wktree.rb
+++ Formula/wktree.rb
@@ -2,8 +2,8 @@ class Wktree < Formula
   desc "Deterministic git worktree manager"
   homepage "https://github.com/codethread/wktree"
   url "https://github.com/codethread/wktree.git",
-      tag:      "v0.2.0",
-      revision: "e6f3ca044ce65a7f013d40611bdc4421e72b09af"
+      tag:      "v0.2.1",
+      revision: "f40979e1744ffd822cc42ef6b273fcefca772581"
 
   depends_on "bun"
   depends_on "git"
